036dd7b2293746e44636c4b24634e31fbd5bee36
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-05-20 16:39:25 +0100

    refactor the server.js to use config port

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+var config = require('./config/config');
 var express = require('express');
 var app = express();
-app.listen(3000, function(){
-  console.log("App listening on port 3000");
+app.listen(config.port, function(){
+  console.log("App listening on port "+config.port);
 });
 app.use('/',function (req,res){
   res.send("Hi you app Running");
 });
+
